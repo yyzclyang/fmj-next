e759d6565d16a6307895f2a2b91c4e25f52cd3fb
loongw <wy721@qq.com>
2018-08-19 23:21:54 +0800

    Version 004

--- src/fmj/Game.kt
+++ src/fmj/Game.kt
@@ -11,7 +11,7 @@ import graphics.Bitmap
 import java.*
 
 class MainGame: Game {
-    val version  = "003"
+    val version  = "004"
     private  val canvas = Canvas(Bitmap(Global.SCREEN_WIDTH, Global.SCREEN_HEIGHT))
     private  val screenStack = ScreenStack(this)
 
